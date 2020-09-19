8a457a8e26928ac45d0b6c0fb26b251bf1b83881
George Zhihua Yuan <yuangeorge9981@gmail.com>
2020-09-18 22:29:34 -0400

    fixed endpoint

--- controllers/LabelGroupController.js
+++ controllers/LabelGroupController.js
@@ -20,7 +20,6 @@ controller.createLabelGroup = async (req, res) => {
 controller.getLabelGroups = async (req, res) => {
   try {
     const labelGroups = await LabelGroup.find()
-      .populate("labels")
 
     if (!labelGroups) {
       res.status(404).send()
